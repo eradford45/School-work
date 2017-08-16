2aa5d412b378b358d0ad2105fd4d3837be1e27f9
Evan Radford <Eradford45@gmail.com>
2017-08-16 11:34:04 -0600

    completed git command hekp program

--- main.rb
+++ main.rb
@@ -1,4 +1,26 @@
-puts 'Learning Git'
-first_name = 'Evan'
-last_name = 'Radford'
-puts "Hello #{first_name} #{last_name}"
+def puts_git (cmd)
+    puts `git #{cmd} -h`
+end
+
+def menu
+    puts '1: Enter Git Command'
+    
+    
+    puts '2: Exit'
+    choice = gets.to_i
+    case choice
+    when 1
+        puts 'Enter a Git Command'
+        puts_git(gets.strip)
+    when 2
+        puts 'Thanks for using the Git Commmand'
+        exit
+    else
+        puts 'invalid input'
+        menu
+    end
+    menu
+end
+
+menu
+
